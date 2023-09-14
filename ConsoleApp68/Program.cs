c189825097acce7f295355236087f81a05af9cea
varashus <rigodavid100@gmail.com>
2023-09-14 10:20:19 +0200

    Add project files.

---
+++ ConsoleApp68/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace CA230914_01
+{
+    class Program
+    {
+        static void Main()
+        {
+            var versenyzok = new List<Versenyzo>();
+            using var sr = new StreamReader(
+                @"..\..\..\src\fob2016.txt",
+                Encoding.UTF8);
+            while (!sr.EndOfStream) versenyzok.Add(new Versenyzo(sr.ReadLine()));
+
+            Console.WriteLine($"3. feladat: Versenyzők száma: {versenyzok.Count}");
+
+            int nokSzama = versenyzok.Count(v => !v.Kategoria);
+            float nokAranya = nokSzama / (float)versenyzok.Count * 100;
+            Console.WriteLine($"4. feladat: Női versenyzpők aránya: {nokAranya:0.00}%");
+            Console.WriteLine("6. feladat: A bajnok női verszenyző");
+
+            var noiBajnok = versenyzok
+                .Where(v => !v.Kategoria)
+                .OrderBy(v => v.Osszpont)
+                .Last();
+            Console.WriteLine($"\tNév: {noiBajnok.Nev}");
+            Console.WriteLine($"\tEgyesület: {noiBajnok.Egyesulet}");
+            Console.WriteLine($"\tKözpont: {noiBajnok.Osszpont}");
+
+
+            using var sw = new StreamWriter(
+                path: @"..\..\..\src\osszpontszam.txt",
+                append: false,
+                encoding: Encoding.UTF8);
+            foreach (var v in versenyzok)
+            {
+                if (v.Kategoria)
+                {
+                    sw.WriteLine($"{v.Nev};{v.Osszpont}");
+                }
+            }
+                            
+
+                
+
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ ConsoleApp68/Versenyzo.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+
+namespace CA230914_01
+{
+    class Versenyzo
+    {
+        public string Nev { get; set; }
+        public bool Kategoria { get; set; }
+        public string Egyesulet { get; set; }
+        public int[] Pontszamok { get; set; }
+
+        public int Osszpont
+        {
+            get
+            {
+                int[] rendezett = Pontszamok.OrderBy(p => p).ToArray();
+                int op = 0;
+                if (rendezett[0] > 0) op += 10;
+                if (rendezett[1] > 0) op += 10;
+                for (int i = 2; i < rendezett.Length; i++)
+                    op += rendezett[i];
+                return op;
+            }
+        }
+
+        public Versenyzo(string r)
+        {
+            string[] v = r.Split(';');
+            Nev = v[0];
+            Kategoria = v[1] == "Felnott ferfi";
+            Egyesulet = v[2] == "n.a." ? null : v[2];
+            Pontszamok = new int[8];
+            for (int i = 0; i < Pontszamok.Length; i++)
+                Pontszamok[i] = int.Parse(v[i + 3]);
+        }
+    }
+}
